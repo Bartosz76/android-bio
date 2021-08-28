f746e6c5c758dd2d68cdc39291a7d76d7f95cb15
Bartosz76 <mierzwinski.b@gmail.com>
2021-08-28 20:15:26 +0200

    Added the option to add hobbies which are then displayed on the screen.

--- app/src/main/java/bm/app/android_bio/MainActivity.java
+++ app/src/main/java/bm/app/android_bio/MainActivity.java
@@ -3,12 +3,25 @@ package bm.app.android_bio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterHobbies;
+    private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterHobbies = findViewById(R.id.enterHobbies);
+        hobbies = findViewById(R.id.hobbiesText);
+    }
+
+    public void addHobbies(View view) {
+        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
+        hobbies.setVisibility(View.VISIBLE);
     }
 }
